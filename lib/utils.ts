5635fa986fee627076b4050c404411f1dc74562c
Naman Sharma <namansharma3007c@gmail.com>
2025-01-12 00:29:23 +0530

    shad cn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,44 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-import { toast } from "@/hooks/use-toast";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function formatDate(inputDate: string | undefined): string | undefined {
-  if (!inputDate) return inputDate;
-  const date = new Date(inputDate);
-
-  const month = date.toLocaleString("default", { month: "short" });
-  const day = date.getDate();
-  const year = date.getFullYear();
-
-  return `${day} ${month} ${year}`;
-}
-
-export async function getUserData() {
-  try {
-    const response = await fetch(`/api/users/get-user/`, {
-      method: "GET",
-    });
-    const data = await response.json();
-    if (!response.ok) {
-      toast({
-        title: data.message,
-        variant: "destructive",
-        duration: 2000,
-      });
-      return null;
-    }
-    return data;
-  } catch (error) {
-    toast({
-      title: "Internal server error!",
-      description: "Please try again later",
-      variant: "destructive",
-      duration: 2000,
-    });
-    return null;
-  }
+  return twMerge(clsx(inputs))
 }
